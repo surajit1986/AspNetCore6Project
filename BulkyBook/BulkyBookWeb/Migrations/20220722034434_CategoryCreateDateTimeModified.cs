7083043d90559fae8808fa85189b13acafd2f993
surajit1986 <surajit.1986@gmail.com>
2022-07-22 09:16:26 +0530

    Initial Commit with project structure and EF Core setup

---
+++ BulkyBook/BulkyBookWeb/Migrations/20220722034434_CategoryCreateDateTimeModified.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBookWeb.Migrations
+{
+    public partial class CategoryCreateDateTimeModified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreaatedDatetime",
+                table: "Categories",
+                newName: "CreatedDatetime");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedDatetime",
+                table: "Categories",
+                newName: "CreaatedDatetime");
+        }
+    }
+}
